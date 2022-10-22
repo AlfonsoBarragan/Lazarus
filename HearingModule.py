71f42f259c90e489e43c8d3851280f6262f16e92
Alfonso Barragán Carmona <alf@MacBook-Pro-de-Alfonso.local>
2022-10-22 14:06:21 +0200

    Generalizing the module to accept variable input

--- HearingModule.py
+++ HearingModule.py
@@ -5,11 +5,16 @@ del gestor asistencial pasivo lazarus.
 
 import whisper
 
-model = whisper.load_model("base")
+def load_whisper(model_kind="base"):
+    return whisper.load_model(model_kind)
+
+def prepare_audio(sound_input):
+    audio = whisper.load_audio("sound/sabina.mp3")
+    audio = whisper.pad_or_trim(audio)
+
 
 # load audio and pad/trim it to fit 30 seconds
-audio = whisper.load_audio("sound/test1.mp3")
-audio = whisper.pad_or_trim(audio)
+
 
 # make log-Mel spectrogram and move to the same device as the model
 mel = whisper.log_mel_spectrogram(audio).to(model.device)
@@ -19,8 +24,6 @@ _, probs = model.detect_language(mel)
 print(f"Detected language: {max(probs, key=probs.get)}")
 
 # decode the audio
-options = whisper.DecodingOptions()
+options = whisper.DecodingOptions(language="es", without_timestamps=True, fp16 = False)
 result = whisper.decode(model, mel, options)
-
-# print the recognized text
 print(result.text)
